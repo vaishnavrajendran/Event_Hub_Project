1d4d34a265c94643eaf1aaa3d677d4a6ea80f1b1
Vaishnav R <vaishnavrajendran95@gmail.com>
2023-02-07 10:41:47 +0530

    backend updated

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 30
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 30
+    },
+    email:{
+        type: String,
+        required: true,
+        max: 30,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5
+    },
+    picturePath: {
+        type: String,
+        default: ''
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+    },
+    {timestamps: true}
+);
+
+const User = mongoose.model('User',UserSchema)
+export default User;
