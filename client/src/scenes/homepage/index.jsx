b92b50963d74a93dd4e049cb6b951a3aa23933bd
Vaishnav R <vaishnavrajendran95@gmail.com>
2023-02-07 20:18:41 +0530

    Navbar added

---
+++ client/src/scenes/homepage/index.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+
+const HomePage = () => {
+    return <Box>
+        <Navbar></Navbar>
+    </Box>
+};
+
+export default HomePage;
